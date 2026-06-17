752da13fe576977db0ab5aeb862d0f8a586e3b57
Aitor Guevara <428243+aitorciki@users.noreply.github.com>
2026-06-18 00:49:07 +0200

    chore: prepare v1.2.1 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -117,8 +117,8 @@ android {
         applicationId = "net.aitorciki.dem3ux"
         minSdk = 26
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.2.0"
+        versionCode = 4
+        versionName = "1.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
