56bb878e2824598f2db9db7b20e957e73a56f421
Aitor Guevara <428243+aitorciki@users.noreply.github.com>
2026-06-24 23:02:27 +0200

    fix: enable edge-to-edge display in MainActivity

--- app/src/main/java/net/aitorciki/dem3ux/MainActivity.kt
+++ app/src/main/java/net/aitorciki/dem3ux/MainActivity.kt
@@ -3,11 +3,13 @@ package net.aitorciki.dem3ux
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import net.aitorciki.dem3ux.ui.Dem3uxApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
 
         setContent {
             Dem3uxApp()
--- app/src/main/java/net/aitorciki/dem3ux/ui/screens/playlists/PlaylistListScreen.kt
+++ app/src/main/java/net/aitorciki/dem3ux/ui/screens/playlists/PlaylistListScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -118,6 +119,7 @@ internal fun PlaylistList(
                 expanded = false,
                 onExpandedChange = {},
                 modifier = Modifier.fillMaxWidth(),
+                windowInsets = WindowInsets(0, 0, 0, 0),
             ) {}
             Spacer(modifier = Modifier.height(12.dp))
             LazyColumn(verticalArrangement = Arrangement.spacedBy(ListItemGap)) {
